455fe4dd4da2af981ffbf72b7fe67db6b50c3c72
SWATHY-project <82394567+SWATHY-project@users.noreply.github.com>
2021-04-13 11:45:55 +0530

    for mycaptian

---
+++ main (1).cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum1= 100 + 5000000;
+    int sum2= sum1 + 89798989808;
+    int sum3= sum2 + sum2;
+    cout<< sum1 <<"\n";
+    cout<< sum2 <<"\n";
+    cout<< sum3;
+    return 0;
+}
